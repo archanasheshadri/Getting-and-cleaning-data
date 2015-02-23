86ce80e1951915bcd6d97e8d6f34c6e8f92ef753
archanasheshadri <archanasheshadri@users.noreply.github.com>
2015-02-22 19:02:08 -0500

    Create run_analysis.r

---
+++ run_analysis.r
@@ -0,0 +1,76 @@
+##Creates R script that does the following. 
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names. 
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+## Get the features (column names)
+features <- read.table("C:\\Coursera\\Getting and Cleaning data\\UCI HAR Dataset\\features.txt")
+col_names <- features[,2]
+
+## Get the activity labels
+activity <- read.table("C:\\Coursera\\Getting and Cleaning data\\UCI HAR Dataset\\activity_labels.txt")
+
+## Get training data
+x_train <- read.table("C:\\Coursera\\Getting and Cleaning data\\UCI HAR Dataset\\train\\X_train.txt")
+y_train <- read.table("C:\\Coursera\\Getting and Cleaning data\\UCI HAR Dataset\\train\\y_train.txt")
+subject_train <- read.table("C:\\Coursera\\Getting and Cleaning data\\UCI HAR Dataset\\train\\subject_train.txt")
+names(subject_train) <- "Subject"
+
+## Name the columns for X_train 
+names(x_train) <- col_names
+
+##Match the activity with activity labels
+activity_labels <- activity[,2]
+y_train[,2] <- activity_labels[y_train[,1]]
+
+
+##Name the activity and activity labels and subject
+names(y_train) <- c("Activity", "Activity_labels")
+y_train <- y_train[c("Activity_labels", "Activity")]
+
+#Get only the Mean and SD columns
+get_meansd_train <- grep("mean|sd", colnames(x_train), value = TRUE)
+meansd_train <- x_train[,get_meansd_train]
+
+
+##Combine training data
+train_data <- cbind(subject_train,y_train, meansd_train)
+
+## Get the test data
+
+x_test <- read.table("C:\\Coursera\\Getting and Cleaning data\\UCI HAR Dataset\\test\\X_test.txt")
+y_test <- read.table("C:\\Coursera\\Getting and Cleaning data\\UCI HAR Dataset\\test\\y_test.txt")
+subject_test <- read.table("C:\\Coursera\\Getting and Cleaning data\\UCI HAR Dataset\\test\\subject_test.txt")
+names(subject_test) <- "Subject"
+
+## Name the columns for X_test
+names(x_test) <- col_names
+
+##Match the activity with activity labels
+y_test[,2] <- activity_labels[y_test[,1]]
+
+##Name the activity and activity labels and subject
+names(y_test) <- c("Activity", "Activity_labels")
+y_test <- y_test[c("Activity_labels", "Activity")]
+
+#Get only the Mean and SD columns
+get_meansd_test <- grep("mean|sd", colnames(x_test), value = TRUE)
+meansd_test <- x_test[,get_meansd_test]
+
+##Combine test data
+test_data <- cbind(subject_test,y_test, meansd_test)
+
+##Merge training and the test dataset
+final_data <- rbind(train_data, test_data)
+
+## Calculate average of each variable for each activity and each subject.
+average <- aggregate(final_data[,3:ncol(final_data)], by=list(final_data$Subject, final_data$Activity_labels), mean)
+names(average[1:2]) <- c("Subject", "Activity_labels")
+write.table(average, "C:\\Coursera\\Getting and Cleaning data\\UCI HAR Dataset\\tidy.txt") 
+
+
+
+
